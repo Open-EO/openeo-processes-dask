78d2dc9441cd7e1a3df1fbd162c07621fc4fa644
Lukas Weidenholzer <lukas.weidenholzer@eodc.eu>
2022-11-29 16:20:50 +0100

    Update test suite with more flexible fixtures

--- tests/test_aggregate.py
+++ tests/test_aggregate.py
@@ -1,16 +1,19 @@
 import numpy as np
 import pytest
-from openeo_pg_parser_networkx.pg_schema import BoundingBox, TemporalInterval
+from openeo_pg_parser_networkx.pg_schema import TemporalInterval
 
 from openeo_processes_dask.process_implementations.cubes.aggregate import (
     aggregate_temporal_period,
 )
 from openeo_processes_dask.process_implementations.math import mean
-from tests.mockdata import generate_fake_rastercube
+from tests.general_checks import general_output_checks
+from tests.mockdata import create_fake_rastercube
 
 
+@pytest.mark.parametrize("size", [(6, 5, 4, 4)])
+@pytest.mark.parametrize("dtype", [np.float64])
 @pytest.mark.parametrize(
-    "incoming_temporal_extent,period,expected",
+    "temporal_extent,period,expected",
     [
         (["2018-05-01", "2018-05-02"], "hour", 25),
         (["2018-05-01", "2018-06-01"], "day", 32),
@@ -20,25 +23,28 @@ from tests.mockdata import generate_fake_rastercube
         (["2018-01-01", "2018-12-31"], "year", 1),
     ],
 )
-def test_aggregate_temporal_period(incoming_temporal_extent, period, expected):
+def test_aggregate_temporal_period(
+    temporal_extent, period, expected, bounding_box, random_data
+):
     """"""
-    spatial_extent = {
-        "west": 10.45,
-        "east": 10.5,
-        "south": 46.1,
-        "north": 46.2,
-        "crs": "EPSG:4326",
-    }
     bands = ["B02", "B03", "B04", "B08"]
-    temporal_extent = incoming_temporal_extent
 
-    test_data = generate_fake_rastercube(
-        "test",
-        spatial_extent=BoundingBox.parse_obj(spatial_extent),
+    input_cube = create_fake_rastercube(
+        data=random_data,
+        spatial_extent=bounding_box,
         temporal_extent=TemporalInterval.parse_obj(temporal_extent),
         bands=bands,
     )
-    aggregated = aggregate_temporal_period(data=test_data, period=period, reducer=mean)
+    output_cube = aggregate_temporal_period(
+        data=input_cube, period=period, reducer=mean
+    )
 
-    assert len(aggregated.t) == expected
-    assert type(aggregated.t.values[0]) == np.datetime64
+    general_output_checks(
+        input_cube=input_cube,
+        output_cube=output_cube,
+        verify_attrs=True,
+        verify_crs=True,
+    )
+
+    assert len(output_cube.t) == expected
+    assert isinstance(output_cube.t.values[0], np.datetime64)
