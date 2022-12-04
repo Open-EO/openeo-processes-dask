b1e6d100869bf59a94a5fb075d2177ebb20b5729
Lukas Weidenholzer <lukas.weidenholzer@eodc.eu>
2022-12-04 18:49:13 +0100

    add tests for fit_regr_random_forest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,17 +2,32 @@ import importlib
 import inspect
 import logging
 
+import dask_geopandas
+import geopandas as gpd
 import numpy as np
 import pytest
-from openeo_pg_parser_networkx.pg_schema import BoundingBox, TemporalInterval
+from dask.distributed import Client
+from geopandas.geodataframe import GeoDataFrame
+from openeo_pg_parser_networkx.pg_schema import (
+    DEFAULT_CRS,
+    BoundingBox,
+    TemporalInterval,
+)
+from shapely.geometry import Point, Polygon
 
 from openeo_processes_dask.core import ProcessRegistry
+from openeo_processes_dask.process_implementations.data_model import VectorCube
 
 logger = logging.getLogger(__name__)
 
 
 @pytest.fixture
-def random_data(size, dtype, seed=42):
+def dask_client():
+    return Client()
+
+
+@pytest.fixture
+def random_raster_data(size, dtype, seed=42):
     rng = np.random.default_rng(seed)
     data = rng.integers(-100, 100, size=size)
     data = data.astype(dtype)
@@ -54,3 +69,16 @@ def process_registry() -> ProcessRegistry:
         registry[process.__name__] = process
 
     return registry
+
+
+@pytest.fixture
+def vector_data_cube() -> VectorCube:
+    x = np.arange(-1683723, -1683723 + 10, 1)
+    y = np.arange(6689139, 6689139 + 10, 1)
+    df = GeoDataFrame(
+        {"geometry": gpd.points_from_xy(x, y), "value1": x + y, "value2": x * y},
+        crs=DEFAULT_CRS,
+    )
+
+    dask_obj = dask_geopandas.from_geopandas(df, npartitions=1)
+    return dask_obj
--- tests/test_reduce.py
+++ tests/test_reduce.py
@@ -12,9 +12,9 @@ from tests.mockdata import create_fake_rastercube
 
 @pytest.mark.parametrize("size", [(6, 5, 4, 4)])
 @pytest.mark.parametrize("dtype", [np.float32])
-def test_reduce_dimension(temporal_interval, bounding_box, random_data):
+def test_reduce_dimension(temporal_interval, bounding_box, random_raster_data):
     input_cube = create_fake_rastercube(
-        data=random_data,
+        data=random_raster_data,
         spatial_extent=bounding_box,
         temporal_extent=temporal_interval,
         bands=["B02", "B03", "B04", "B08"],
