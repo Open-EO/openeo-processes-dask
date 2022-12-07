91270adefbfaff468780068d1e7c4a79338770b4
clausmichele <31700619+clausmichele@users.noreply.github.com>
2022-12-07 19:24:04 +0100

    Fixed drop_dimension implementation (#14)
    * Fixed drop_dimension implementation
    
    Parameter dimension renamed to name.
    Using .drop instead of drop_dims, which is available only for xArray Datasets
    
    * Added exception
    
    * Reset python version dependency
    
    * lint mods

--- openeo_processes_dask/process_implementations/cubes/general.py
+++ openeo_processes_dask/process_implementations/cubes/general.py
@@ -1,19 +1,21 @@
 import xarray as xr
 from openeo_pg_parser_networkx.pg_schema import *
 
-from openeo_processes_dask.exceptions import DimensionNotAvailable
+from openeo_processes_dask.exceptions import (
+    DimensionLabelCountMismatch,
+    DimensionNotAvailable,
+)
 from openeo_processes_dask.process_implementations.data_model import RasterCube
 
 __all__ = ["create_raster_cube", "drop_dimension", "dimension_labels"]
 
 
-def drop_dimension(data: RasterCube, dimension: str) -> RasterCube:
-    if dimension not in data.dims:
-        raise DimensionNotAvailable(
-            "A dimension with the specified name does not exist."
-        )
-
-    return data.drop_dims(dimension)
+def drop_dimension(data: RasterCube, name: str) -> RasterCube:
+    if name not in data.dims:
+        raise DimensionNotAvailable()
+    if len(data[name]) > 1:
+        raise DimensionLabelCountMismatch()
+    return data.drop(name)
 
 
 def create_raster_cube() -> RasterCube:
@@ -22,7 +24,5 @@ def create_raster_cube() -> RasterCube:
 
 def dimension_labels(data: RasterCube, dimension: str) -> RasterCube:
     if dimension not in data.dims:
-        raise DimensionNotAvailable(
-            "A dimension with the specified name does not exist."
-        )
+        raise DimensionNotAvailable()
     return data.coords[dimension]
