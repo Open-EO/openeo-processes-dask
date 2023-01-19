1d6a6d6fffe03a11a9f5e9bd1d99f2ba8556e040
Lukas Weidenholzer <lukas.weidenholzer@eodc.eu>
2023-01-19 16:23:46 +0100

    first draft of apply_dimension

--- openeo_processes_dask/process_implementations/cubes/apply.py
+++ openeo_processes_dask/process_implementations/cubes/apply.py
@@ -5,7 +5,7 @@ import xarray as xr
 from openeo_processes_dask.exceptions import DimensionNotAvailable
 from openeo_processes_dask.process_implementations.data_model import RasterCube
 
-__all__ = ["apply"]
+__all__ = ["apply", "apply_dimension"]
 
 
 def apply(
@@ -33,12 +33,32 @@ def apply_dimension(
     context: Optional[dict] = None,
     **kwargs,
 ) -> RasterCube:
+    if context is None:
+        context = {}
+
     if dimension not in data.dims:
         raise DimensionNotAvailable(
             f"Provided dimension ({dimension}) not found in data.dims: {data.dims}"
         )
 
-    parameters = {"data": data, "context": context}
+    if target_dimension is None:
+        target_dimension = dimension
 
-    output_data = process(parameters=parameters, dimension=dimension)
-    raise NotImplementedError("apply_dimension isn't supported yet!")
+    positional_parameters = {"data": 0}
+    named_parameters = {"context": context}
+
+    data.dims
+    result = xr.apply_ufunc(
+        process,
+        data,
+        input_core_dims=[[dimension]],
+        output_core_dims=[[target_dimension]],
+        dask="allowed",
+        kwargs={
+            "positional_parameters": positional_parameters,
+            "named_parameters": named_parameters,
+        },
+    )
+
+    transposed_result = result.transpose(*data.dims)
+    return transposed_result
--- tests/test_apply.py
+++ tests/test_apply.py
@@ -5,7 +5,10 @@ import pytest
 import xarray as xr
 from openeo_pg_parser_networkx.pg_schema import ParameterReference
 
-from openeo_processes_dask.process_implementations.cubes.apply import apply
+from openeo_processes_dask.process_implementations.cubes.apply import (
+    apply,
+    apply_dimension,
+)
 from tests.general_checks import assert_numpy_equals_dask_numpy, general_output_checks
 from tests.mockdata import create_fake_rastercube
 
@@ -36,3 +39,31 @@ def test_apply(temporal_interval, bounding_box, random_raster_data, process_regi
     )
 
     xr.testing.assert_equal(output_cube, input_cube + 1)
+
+
+@pytest.mark.parametrize("size", [(6, 5, 4, 4)])
+@pytest.mark.parametrize("dtype", [np.float32])
+def test_apply_dimension(
+    temporal_interval, bounding_box, random_raster_data, process_registry
+):
+    input_cube = create_fake_rastercube(
+        data=random_raster_data,
+        spatial_extent=bounding_box,
+        temporal_extent=temporal_interval,
+        bands=["B02", "B03", "B04", "B08"],
+        backend="dask",
+    )
+
+    _process = partial(
+        process_registry["add"], y=1, x=ParameterReference(from_parameter="data")
+    )
+
+    output_cube = apply_dimension(data=input_cube, process=_process, dimension="x")
+
+    general_output_checks(
+        input_cube=input_cube,
+        output_cube=output_cube,
+        verify_attrs=True,
+        verify_crs=True,
+        expected_results=(input_cube + 1),
+    )
