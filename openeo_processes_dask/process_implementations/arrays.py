2d92c13c331df77e1278743355b4db376c15353b
ValentinaHutter <valentinahutter@gmail.com>
2023-01-20 14:12:08 +0100

    update array element for arrays

--- openeo_processes_dask/process_implementations/arrays.py
+++ openeo_processes_dask/process_implementations/arrays.py
@@ -35,8 +35,12 @@ def array_element(
         return element
 
     if index is not None:
-        element = data.isel({dimension: int(index)})
-        return element
+        if dimension is not None: 
+            element = data.isel({dimension: int(index)})
+            return element
+        else: 
+            element = data[int(index)]
+            return element
 
     raise ValueError("Shouldn't have come here!")
 
---
+++ tests/test_array.py
@@ -0,0 +1,39 @@
+from functools import partial
+
+import numpy as np
+import pytest
+import xarray as xr
+from openeo_pg_parser_networkx.pg_schema import ParameterReference
+
+from openeo_processes_dask.process_implementations.cubes.apply import apply
+from tests.general_checks import assert_numpy_equals_dask_numpy, general_output_checks
+from tests.mockdata import create_fake_rastercube
+
+
+@pytest.mark.parametrize("size", [(6, 5, 4, 4)])
+@pytest.mark.parametrize("dtype", [np.float32])
+def test_array_element(temporal_interval, bounding_box, random_raster_data, process_registry):
+    input_cube = create_fake_rastercube(
+        data=random_raster_data,
+        spatial_extent=bounding_box,
+        temporal_extent=temporal_interval,
+        bands=["B02", "B03", "B04", "B08"],
+        backend="dask",
+    )
+
+    _process = partial(
+        process_registry["array_element"], index=1, x=ParameterReference(from_parameter="x")
+    )
+
+    output_cube = apply(data=input_cube, process=_process)
+
+    general_output_checks(
+        input_cube=input_cube,
+        output_cube=output_cube,
+        verify_attrs=True,
+        verify_crs=True,
+        expected_results=(input_cube[1]),
+    )
+
+    xr.testing.assert_equal(output_cube, input_cube[1])
+    print(output_cube)
