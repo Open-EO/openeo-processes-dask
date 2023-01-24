459878943a58c2b028194e56b78ba195f1a30882
ValentinaHutter <valentinahutter@gmail.com>
2023-01-24 10:51:23 +0100

    update array processes and tests

--- openeo_processes_dask/process_implementations/arrays.py
+++ openeo_processes_dask/process_implementations/arrays.py
@@ -1,5 +1,7 @@
 from typing import Callable, Optional, Union
 
+import numpy as np
+import pandas as pd
 import xarray as xr
 
 from openeo_processes_dask.exceptions import (
@@ -8,7 +10,23 @@ from openeo_processes_dask.exceptions import (
 )
 from openeo_processes_dask.process_implementations.data_model import RasterCube
 
-__all__ = ["array_element", "array_filter", "count"]
+__all__ = [
+    "array_element", 
+    "array_filter", 
+    "count", 
+    "array_create", 
+    "array_modify", 
+    "array_concat", 
+    "array_contains", 
+    "array_apply", 
+    "array_find", 
+    "array_labels", 
+    "first", 
+    "last", 
+    "order", 
+    "rearrange", 
+    "sort",
+    ]
 
 
 def array_element(
@@ -60,3 +78,238 @@ def count(data: RasterCube, condition: Callable, **kwargs):
     else:
         data = data.sum()
     return data
+
+
+def array_create(
+    data: Union[np.array, list, float, int], 
+    repeat: Optional[int] = 1,
+    **kwargs
+    ):
+    if type(data) in [int, float]:
+        data = [data]
+    if not hasattr(data,"__array_interface__"):
+        data = np.array(data)
+    if len(data) == 0:
+        return np.array([])
+    return np.tile(data, reps=repeat)
+
+
+def array_modify(
+    data: Union[np.array, list], 
+    values: Union[np.array, list], 
+    index: int, 
+    length: Optional[int] = 1,
+    **kwargs
+    ):
+    if not hasattr(data,"__array_interface__"):
+        data = np.array(data)
+    if not hasattr(values,"__array_interface__"):
+        values = np.array(values)
+    if index == 0:
+        modified = values
+    else:
+        first = data[:index]
+        modified = np.append(first, values)
+    if index+length < len(data):
+        modified = np.append(modified, data[index+length:])
+    return modified
+
+
+def array_concat(
+    array1: Union[np.array, list], 
+    array2: Union[np.array, list], 
+    **kwargs
+    ):
+    if not hasattr(array1,"__array_interface__"):
+        array1 = np.array(array1)
+    if not hasattr(array2,"__array_interface__"):
+        array2 = np.array(array2)
+    concat = np.append(array1, array2)
+    return concat
+
+
+def array_contains(
+    data: Union[np.array, list], 
+    value: Union[int, float, str, list]
+    ):
+    if np.array(pd.isnull(value)).all():
+        return np.isnan(data).any()
+    else: 
+        return np.isin(data, value).any()
+
+
+def array_apply(
+    data: Union[np.array, list], 
+    process: Callable, 
+    context: Optional[dict] = None,
+    **kwargs
+    ):
+    context = context if context is not None else {}
+    if not hasattr(data,"__array_interface__"):
+        data = np.array(data)
+    return process(data, **context)
+
+
+def array_find(
+    data: Union[np.array, list], 
+    value: float, 
+    reverse: Optional[bool] = False,
+    dimension: Optional[int] = None
+    ):
+    if not hasattr(data,"__array_interface__"):
+        data = np.array(data)
+    if np.isnan(value) or len(data) == 0:
+        return np.nan
+    else:
+        idxs = np.argmax((data == value), axis=dimension)
+    if reverse:
+        b = np.flip(data, axis=dimension)
+        idxs = np.shape(b)[dimension] - np.argmax((b == value), axis=dimension) - 1
+    return idxs
+
+
+def array_labels(
+    data: Union[np.array, list], 
+    dimension: Optional[int] = None
+    ):
+    if not hasattr(data,"__array_interface__"):
+        data = np.array(data)
+    if dimension is None:
+        n_vals = len(data)
+    if isinstance(dimension, int):
+        n_vals = np.shape(data)[dimension]
+    return np.arange(n_vals)
+
+
+def first(
+    data: Union[np.array, list], 
+    ignore_nodata: Optional[bool] = True, 
+    dimension: Optional[str] = None
+    ):
+    if not hasattr(data,"__array_interface__"):
+        data = np.array(data)
+    if len(data) == 0: 
+        return np.nan
+    if dimension is None:
+        dimension = 0
+    n_dims = len(data.shape)
+    if ignore_nodata:  # skip np.nan values
+        nan_mask = ~pd.isnull(data)  # create mask for valid values (not np.nan)
+        idx_first = np.argmax(nan_mask, axis=dimension)
+        first_elem = np.take_along_axis(data, np.expand_dims(idx_first, axis=dimension), axis=dimension)
+    else:  # take the first element, no matter np.nan values are in the array
+        slices = [slice(None)] * n_dims
+        slices[dimension] = 0
+        idx_first = tuple(slices)
+        first_elem = data[idx_first]
+
+    return first_elem
+
+
+def last(
+    data: Union[np.array, list], 
+    ignore_nodata: Optional[bool] = True, 
+    dimension: Optional[str] = None
+    ):
+    if not hasattr(data,"__array_interface__"):
+        data = np.array(data)
+    if len(data) == 0: 
+        return np.nan
+    if dimension is None:
+        dimension = 0
+    n_dims = len(data.shape)
+    if ignore_nodata:  # skip np.nan values
+        data = np.flip(data, axis=dimension)  # flip data to retrieve the first valid element (thats the only way it works with argmax)
+        last_elem = first(data, ignore_nodata=ignore_nodata, dimension=dimension)
+    else:  # take the first element, no matter np.nan values are in the array
+        slices = [slice(None)] * n_dims
+        slices[dimension] = -1
+        idx_last = tuple(slices)
+        last_elem = data[idx_last]
+
+    return last_elem
+
+
+def order(
+    data: Union[np.array, list], 
+    asc: Optional[bool] = True, 
+    nodata: Optional[bool] = True, 
+    dimension: Optional[int] = None
+    ):
+    if not hasattr(data,"__array_interface__"):
+        data = np.array(data)
+    if len(data) == 0: 
+        return np.nan
+    if dimension is None: 
+        dimension = 0
+    if asc:
+        permutation_idxs = np.argsort(data, kind='mergesort', axis=dimension)
+    else:  # [::-1] not possible
+        permutation_idxs = np.argsort(-data, kind='mergesort', axis=dimension)  # to get the indizes in descending order, the sign of the data is changed
+
+    if nodata is None:  # ignore np.nan values
+        # sort the original data first, to get correct position of no data values
+        sorted_data = data[permutation_idxs]
+        return permutation_idxs[~pd.isnull(sorted_data)]
+    elif nodata is False:  # put location/index of np.nan values first
+        # sort the original data first, to get correct position of no data values 
+        sorted_data = data[permutation_idxs]
+        nan_idxs = pd.isnull(sorted_data)
+
+        # flip permutation and nan mask
+        permutation_idxs_flip = np.flip(permutation_idxs, axis=dimension)
+        nan_idxs_flip = np.flip(nan_idxs, axis=dimension)
+
+        # flip causes the nan.values to be first, however the order of all other values is also flipped
+        # therefore the non np.nan values (i.e. the wrong flipped order) is replaced by the right order given by
+        # the original permutation values
+        permutation_idxs_flip[~nan_idxs_flip] = permutation_idxs[~nan_idxs]
+
+        return permutation_idxs_flip
+    elif nodata is True:  # default argsort behaviour, np.nan values are put last
+        return permutation_idxs
+    else:
+        err_msg = "Data type of 'nodata' argument is not supported."
+        raise Exception(err_msg)
+
+
+def rearrange(
+    data: Union[np.array, list], 
+    order
+    ):
+    if not hasattr(data,"__array_interface__"):
+        data = np.array(data)
+    if len(data) == 0: 
+        return np.nan
+    return data[order]
+
+
+def sort(
+    data: Union[np.array, list]  , 
+    asc: Optional[bool] = True,
+    nodata: Optional[bool] = None,
+    dimension: Optional[int] = None
+    ):
+    if not hasattr(data,"__array_interface__"):
+        data = np.array(data)
+    if len(data) == 0: 
+        return np.nan
+    if asc:
+        data_sorted = np.sort(data, axis=dimension)
+    else:  # [::-1] not possible
+        data_sorted = -np.sort(-data, axis=dimension)  # to get the indexes in descending order, the sign of the data is changed
+
+    if nodata is None:  # ignore np.nan values
+        nan_idxs = pd.isnull(data_sorted)
+        return data_sorted[~nan_idxs]
+    elif nodata == False:  # put np.nan values first
+        nan_idxs = pd.isnull(data_sorted)
+        data_sorted_flip = np.flip(data_sorted, axis=dimension)
+        nan_idxs_flip = pd.isnull(data_sorted_flip)
+        data_sorted_flip[~nan_idxs_flip] = data_sorted[~nan_idxs]
+        return data_sorted_flip
+    elif nodata == True:  # default sort behaviour, np.nan values are put last
+        return data_sorted
+    else:
+        err_msg = "Data type of 'nodata' argument is not supported."
+        raise Exception(err_msg)
--- tests/test_array.py
+++ tests/test_array.py
@@ -3,9 +3,9 @@ from functools import partial
 import numpy as np
 import pytest
 import xarray as xr
-from openeo_pg_parser_networkx.pg_schema import ParameterReference
+from openeo_pg_parser_networkx.pg_schema import ParameterReference, BoundingBox, TemporalInterval
 
-from openeo_processes_dask.process_implementations.arrays import array_element
+from openeo_processes_dask.process_implementations.arrays import *
 from tests.general_checks import assert_numpy_equals_dask_numpy, general_output_checks
 from tests.mockdata import create_fake_rastercube
 
@@ -39,3 +39,88 @@ def test_array_element(temporal_interval, bounding_box, random_raster_data, proc
 
     assert output_array == 4
     assert output_array == array_element(data=input_array, index=-1)
+
+def test_array_create():
+    assert (array_create(2,3) == np.array([2,2,2])).all()
+    assert (len(array_create([])) == 0)
+    assert (array_create(np.array([1, 2, 3]), repeat=2) == np.array([1,2,3,1,2,3])).all()
+    assert (array_create(["A", "B", "C"]) == np.array(["A", "B", "C"])).all()
+    assert len(array_create([np.nan, 1], 2)) == 4
+
+def test_array_modify():
+    assert (array_modify(np.array([2,3]), np.array([4,7]), 1, 0) == np.array([2,4,7,3])).all()
+    assert (array_modify(data = ["a","d","c"], values = ["b"], index = 1) == ['a','b','c']).all()
+    assert (array_modify(data = ["a","c"], values = ["b"], index = 1, length = 0) == ["a","b","c"]).all()
+    assert (array_modify(data = [np.nan,np.nan,"a","b","c"], values = [], index = 0, length = 2) == ["a","b","c"]).all()
+    assert (array_modify(data=["a", "b", "c"], values=[], index=1, length=10) == ["a"])
+
+def test_array_concat():
+    assert (array_concat(np.array([2, 3]), np.array([4, 7]))== np.array([2,3,4,7])).all()
+    assert (array_concat(array1 = ["a","b"], array2 = [1,2]) == np.array(['a','b','1','2'], dtype=object)).all()
+
+def test_array_contains():
+    assert array_contains([1, 2, 3], value=2)
+    assert not array_contains(["A", "B", "C"], value="b")
+    assert not array_contains([1, 2, 3], value="2")
+    assert array_contains([1, 2, 3], value=2)
+    assert array_contains([1, 2, np.nan], value=np.nan)
+    assert array_contains([[1, 2], [3, 4]], value=[1, 2])
+    assert array_contains([[1, 2], [3, 4]], value=2)
+    assert array_contains([{"a": "b"}, {"c": "d"}], value={"a": "b"})
+
+def test_array_find():
+    assert array_find([1, 0, 3, 2], value= 3) == 2
+    assert np.isnan(array_find([1, 0, 3, 2, np.nan, 3], value = np.nan))
+    data = np.array([[2, 8, 2, 4], [0, np.nan, 2, 2]])
+    assert (array_find(data, value = 2, dimension = 1) == [0,2]).all()
+    assert (array_find(data, value = 2, reverse = True, dimension = 1) == [2,3]).all()
+
+def test_array_labels():
+    """ Tests `array_labels` function. """
+    assert (array_labels([1, 0, 3, 2]) == np.array([0, 1, 2, 3])).all()
+    assert (array_labels([[1, 0, 3, 2]], dimension = 0) == np.array([0]))
+
+def test_first():
+    assert first([1, 0, 3, 2]) == 1
+    assert np.isnan(first([np.nan, 2, 3], ignore_nodata=False))
+    assert np.isnan(first([]))
+
+    test_arr = np.array([[[np.nan, 2], [1, 2]], [[3, 2], [1, 2]], [[1, 2], [1, np.nan]]])
+    first_elem_ref = np.array([[[3., 2.], [1., 2.]]])
+    first_elem = first(test_arr)
+    assert np.isclose(first_elem, first_elem_ref, equal_nan=True).all()
+    test_arr = np.array([[[np.nan, 2], [1, 2]], [[3, 2], [1, 2]], [[1, 2], [1, np.nan]]])
+    first_elem_ref = np.array([[[np.nan, 2.], [1., 2.]]])
+    first_elem = first(test_arr, ignore_nodata=False)
+    assert np.isclose(first_elem, first_elem_ref, equal_nan=True).all()
+
+def test_last():
+    assert last([1, 0, 3, 2]) == 2
+    assert np.isnan(last([0, 1, np.nan], ignore_nodata=False))
+    assert np.isnan(last([]))
+
+    test_arr = np.array([[[np.nan, 2], [1, 2]], [[3, 2], [1, 3]], [[1, 2], [1, np.nan]]])
+    last_elem_ref = np.array([[[1., 2.], [1., 3.]]])
+    last_elem = last(test_arr)
+    assert np.isclose(last_elem, last_elem_ref, equal_nan=True).all()
+    test_arr = np.array([[[np.nan, 2], [1, 2]], [[3, 2], [1, 2]], [[1, 2], [1, np.nan]]])
+    last_elem_ref = np.array([[[1., 2.], [1., np.nan]]])
+    last_elem = last(test_arr, ignore_nodata=False)
+    assert np.isclose(last_elem, last_elem_ref, equal_nan=True).all()
+
+def test_order():
+    assert (order([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9])==np.array([1, 2, 8, 5, 0, 4, 7, 9, 10, 3, 6])).all()
+    assert (order([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9], nodata=True)==[1, 2, 8, 5, 0, 4, 7, 9, 10, 3, 6]).all()
+    assert (order([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9], asc=False, nodata=True)==[9, 10, 7, 4, 0, 5, 8, 2, 1, 3, 6]).all()
+    assert (order([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9], asc=False, nodata=False)==[6, 3, 9, 10, 7, 4, 0, 5, 8, 2, 1]).all()
+
+def test_rearrange():
+    assert (rearrange([5, 4, 3], [2, 1, 0]) == [3, 4, 5]).all()
+    assert (rearrange([5, 4, 3, 2], [0, 2, 1, 3]) == [5, 3, 4, 2]).all()
+    assert (rearrange([5, 4, 3, 2], [1, 3]) == [4, 2]).all()
+
+def test_sort():
+    """ Tests `sort` function. """
+    assert (sort([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9])==[-1, 2, 3, 4, 6, 7, 8, 9, 9]).all()
+    assert np.isclose(sort([6, -1, 2, np.nan, 7, 4, np.nan, 8, 3, 9, 9], asc=False, nodata=True),
+                        [9, 9, 8, 7, 6, 4, 3, 2, -1, np.nan, np.nan], equal_nan=True).all()
