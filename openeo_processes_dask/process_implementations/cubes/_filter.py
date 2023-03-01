06c73311972844bf85791e4ed47d4f2374bb498a
clausmichele <claus.michele@hotmail.it>
2023-03-01 10:23:31 +0100

    implemented filter_temporal

--- openeo_processes_dask/process_implementations/cubes/_filter.py
+++ openeo_processes_dask/process_implementations/cubes/_filter.py
@@ -1,5 +1,6 @@
 from typing import Callable
 
+import numpy as np
 from openeo_pg_parser_networkx.pg_schema import BoundingBox, GeoJson, TemporalInterval
 
 from openeo_processes_dask.exceptions import DimensionNotAvailable
@@ -15,9 +16,40 @@ def filter_bbox(data: RasterCube, extent: BoundingBox) -> RasterCube:
 
 
 def filter_temporal(
-    data: RasterCube, extent: TemporalInterval, dimension: str
+    data: RasterCube, extent: TemporalInterval, dimension: str = None
 ) -> RasterCube:
-    raise NotImplementedError()
+    temporal_dims = data.openeo.temporal_dims
+
+    if dimension is not None:
+        if dimension not in data.dims:
+            raise DimensionNotAvailable(
+                f"A dimension with the specified name: {dimension} does not exist."
+            )
+        applicable_temporal_dimension = dimension
+    else:
+        if not temporal_dims:
+            raise DimensionNotAvailable(
+                f"No temporal dimension detected on dataset. Available dimensions: {data.dims}"
+            )
+        if len(temporal_dims) > 1:
+            raise TooManyDimensions(
+                f"The data cube contains multiple temporal dimensions: {temporal_dims}. The parameter `dimension` must be specified."
+            )
+    applicable_temporal_dimension = temporal_dims[0]
+
+    start_time = extent[0]
+    if start_time is not None:
+        start_time = start_time.to_numpy()
+    end_time = extent[1]
+    if end_time is not None:
+        end_time = extent[1].to_numpy() - np.timedelta64(1, "ms")
+    # The second element is the end of the temporal interval.
+    # The specified instance in time is excluded from the interval.
+    # See https://processes.openeo.org/#filter_temporal
+
+    filtered = data.loc[{applicable_temporal_dimension: slice(start_time, end_time)}]
+
+    return filtered
 
 
 def filter_labels(
