bee62ccb1a0eb14f9c4269fc490b1161b7d22424
Lukas Weidenholzer <lukas.weidenholzer@eodc.eu>
2023-03-03 14:32:49 +0100

    move xr __init__ to sub __init__

--- openeo_processes_dask/process_implementations/__init__.py
+++ openeo_processes_dask/process_implementations/__init__.py
@@ -13,3 +13,7 @@ try:
     from .ml import *
 except ImportError as e:
     logger.warning("Could not load ML processes due to missing dependencies: ", e)
+
+import rioxarray as rio  # Required for the .rio accessor on xarrays.
+
+import openeo_processes_dask.process_implementations.cubes._xr_interop
