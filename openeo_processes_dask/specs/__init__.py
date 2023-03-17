f782ec8ffcfb16b6b5915d14768f8678573441cf
Lukas Weidenholzer <17790923+LukeWeidenwalker@users.noreply.github.com>
2023-03-17 17:16:50 +0100

    fix: align specs with currently implemented processes (#87)
    * align specs with currently implemented processes
    
    * small change in how specs are exported

--- openeo_processes_dask/specs/__init__.py
+++ openeo_processes_dask/specs/__init__.py
@@ -1,4 +1,6 @@
+import builtins
 import json
+import keyword
 import logging
 from pathlib import Path
 
@@ -10,14 +12,16 @@ process_json_paths = [pg_path for pg_path in (json_path).glob("*.json")]
 # Go through all the jsons in the top-level of the specs folder and add them to be importable from here
 # E.g. from openeo_processes_dask.specs import *
 # This is frowned upon in most python code, but I think here it's fine and allows a nice way of importing these
+
+__all__ = []
+
 for spec_path in process_json_paths:
     spec_json = json.load(open(spec_path))
 
     process_name = spec_json["id"]
     # Make sure we don't overwrite any builtins
-    if spec_json["id"] in globals() or spec_json["id"] in globals()["__builtins__"]:
+    if spec_json["id"] in dir(builtins) or keyword.iskeyword(spec_json["id"]):
         process_name = "_" + spec_json["id"]
 
-    globals()[process_name] = spec_json
-
-print("h")
+    locals()[process_name] = spec_json
+    __all__.append(process_name)
