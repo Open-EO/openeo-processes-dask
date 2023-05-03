232aa6b6e8bb9194a07ca95b9e86349fbd5c458f
Lukas Weidenholzer <lukas.weidenholzer@eodc.eu>
2023-05-03 13:36:33 +0200

    remove unused imports

--- tests/test_dimensions.py
+++ tests/test_dimensions.py
@@ -1,9 +1,5 @@
-from functools import partial
-
 import numpy as np
 import pytest
-import xarray as xr
-from openeo_pg_parser_networkx.pg_schema import ParameterReference
 
 from openeo_processes_dask.process_implementations.cubes.general import add_dimension
 from tests.general_checks import general_output_checks
@@ -12,9 +8,7 @@ from tests.mockdata import create_fake_rastercube
 
 @pytest.mark.parametrize("size", [(30, 30, 20, 4)])
 @pytest.mark.parametrize("dtype", [np.float32])
-def test_add_dimension(
-    temporal_interval, bounding_box, random_raster_data, process_registry
-):
+def test_add_dimension(temporal_interval, bounding_box, random_raster_data):
     input_cube = create_fake_rastercube(
         data=random_raster_data,
         spatial_extent=bounding_box,
