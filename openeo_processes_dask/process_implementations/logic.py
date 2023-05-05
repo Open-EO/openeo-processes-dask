b1ec5fa8bed9053d99da7357c29957ccbaa0b875
Lukas Weidenholzer <lukas.weidenholzer@eodc.eu>
2023-05-05 15:24:03 +0200

    typing

--- openeo_processes_dask/process_implementations/logic.py
+++ openeo_processes_dask/process_implementations/logic.py
@@ -2,8 +2,9 @@ from typing import Any
 
 import numpy as np
 import pandas as pd
+from numpy.typing import ArrayLike
 
 
-def is_valid(x: Any) -> bool:
+def is_valid(x: Any) -> ArrayLike:
     null_mask = pd.isnull(np.asarray(x))
     return ~null_mask
