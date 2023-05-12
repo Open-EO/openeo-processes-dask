8d474fbba5f5bfe16940c9a957ff74ef8401944b
sean <sean.hoyal@eodc.eu>
2023-05-12 16:19:16 +0200

    update function to handle spatial resampling as well as reprojection

--- openeo_processes_dask/process_implementations/cubes/resample.py
+++ openeo_processes_dask/process_implementations/cubes/resample.py
@@ -1,94 +1,119 @@
-import odc.algo
+from typing import Union
 
-from affine import Affine
-from enum import Enum
-from datacube.utils.geometry import GeoBox
-from math import pi
+import odc.algo
 from pyproj import Transformer
 from pyproj.crs import CRS, CRSError
 
-from typing import Union, Optional
-
-from openeo_processes_dask.process_implementations.cubes.utils import detect_changing_unit
+from openeo_processes_dask.process_implementations.cubes.utils import (
+    detect_changing_unit,
+)
 from openeo_processes_dask.process_implementations.data_model import RasterCube
 
-__all__ = ["resample_spatial"]
+resample_methods_list = [
+    "near",
+    "bilinear",
+    "cubic",
+    "cubicspline",
+    "lanczos",
+    "average",
+    "mode",
+    "max",
+    "min",
+    "med",
+    "q1",
+    "q3",
+]
 
 
 def resample_spatial(
-        data: RasterCube,
-        epsg_code: Union[str, int],
-        resolution: int = 0,
-        method: str = 'near',
-        align: str = "upper-left"):
-    """ Resample the input rastercube by the provided epsg_code. Currently not accepting resolution resampling. """
-    # Re-order
-    data = data.transpose("bands", "t", "y", "x")
+    data: RasterCube,
+    epsg_code: Union[str, int] = None,
+    resolution: int = None,
+    method: str = "near",
+    align: str = "upper-left",
+):
+    """Resample the input rastercube by the provided epsg_code. Currently not accepting resolution resampling."""
 
-    src_crs = CRS(data.rio.crs)
+    # Assert resampling method is correct.
+    if method == "near":
+        method = "nearest"
 
-    try:
-        dst_crs = CRS.from_epsg(epsg_code)
-    except CRSError:
-        raise Exception(f"epsg_code parameter {epsg_code} is not a valid epsg code.")
-
-    transformer = Transformer.from_crs(
-        src_crs, 
-        dst_crs,
-        always_xy=True,
-    )
-
-    # For the affine, we want to know the x and y coordinate for the upper-left pixel.
-    tmp_left_pixel = data.isel(
-        x=[0],
-        y=[0]
-    )
-    tmp_left_pixel.x.values, tmp_left_pixel.y.values
-    # Transform the upper left pixel coords to the destination crs.
-    new_x, new_y = transformer.transform(
-        tmp_left_pixel.x.values,
-        tmp_left_pixel.y.values
-    )
-
-    resolution = detect_changing_unit(
-        src_crs=src_crs,
-        dst_crs= dst_crs,
-        # First value of affine is pixel width.
-        src_res=data.affine[0]
-    )
-
-    # Docs for geotransform
-    # https://gdal.org/tutorials/geotransforms_tut.html
-    new_affine = Affine(
-        resolution,
-        0,
-        new_x[0],
-        0,
-        # Negative pixel width used for pixel height
-        -resolution,
-        new_y[0]
-    )
-
-    dst_geo = GeoBox(
-        width=len(data.x),
-        height=len(data.y),
-        crs=dst_crs,
-        affine=new_affine
-    )
-
-    reprojected =  odc.algo.xr_reproject(
-        src=data,
-        geobox=dst_geo,
-    )
-    if "longitude" in reprojected.dims:
-        reprojected = reprojected.rename(
-            {"longitude": data.openeo.x_dim}
+    elif method not in resample_methods_list:
+        raise Exception(
+            f'Selected resampling method "{method}" is not available! Please select one of '
+            f"[{', '.join(resample_methods_list)}]"
         )
 
+    # Re-order, this is specifically done for xr_reproject
+    data = data.transpose("bands", "t", "y", "x")
+
+    # Do reprojection first, and then resampling
+    if epsg_code:
+        try:
+            dst_crs = CRS.from_epsg(epsg_code)
+        except CRSError:
+            raise Exception(
+                f"epsg_code parameter {epsg_code} is not a valid epsg code."
+            )
+
+        # Get original crs and resolution from dataset.
+        src_crs = CRS(data.rio.crs)
+
+        transformer = Transformer.from_crs(
+            src_crs,
+            dst_crs,
+            always_xy=True,
+        )
+        # For the affine, we want to know the x and y coordinate for the upper-left pixel.
+        top_left_pixel = data.isel(x=[0], y=[0])
+
+        # Transform the upper left pixel coords to the destination crs.
+        new_x, new_y = transformer.transform(
+            top_left_pixel.x.values, top_left_pixel.y.values
+        )
+
+        # If resolution has not been provided, it must be inferred from old resolution.
+        # First value of affine is pixel width. Use it as "resolution".
+        src_res = data.affine[0]
+        dst_res = detect_changing_unit(
+            src_crs=src_crs,
+            dst_crs=dst_crs,
+            # First value of affine is pixel width, which we use as 'resolution'.
+            src_res=src_res,
+        )
+
+        dst_geobox = prepare_geobox(data, dst_crs, dst_res, src_res, new_x[0], new_y[0])
+        data = odc.algo.xr_reproject(src=data, geobox=dst_geobox)
+        data.attrs["crs"] = dst_crs
+
+    # And if resampling was requested
+    if resolution:
+        dst_res = resolution
+        # Get src_crs seperately incase reprojection was carried out.
+        src_crs = CRS(data.rio.crs)
+        # Get top left pixel seperately incase reprojection was carried out
+        top_left_pixel = data.isel(x=[0], y=[0])
+        src_res = data.affine[0]
+
+        dst_geobox = prepare_geobox(
+            data,
+            src_crs,
+            dst_res,
+            src_res,
+            top_left_pixel.x.values[0],
+            top_left_pixel.y.values[0],
+            scale=True,
+        )
+
+        data = odc.algo.xr_reproject(src=data, geobox=dst_geobox)
+
+    reprojected = data
+
+    if "longitude" in reprojected.dims:
+        reprojected = reprojected.rename({"longitude": data.openeo.x_dim})
+
     if "latitude" in reprojected.dims:
-        reprojected = reprojected.rename(
-            {"latitude": data.openeo.y_dim}
-        )  
-    
-    reprojected.attrs["crs"] = dst_crs
+        reprojected = reprojected.rename({"latitude": data.openeo.y_dim})
+
+    reprojected.attrs["crs"] = data.rio.crs
     return reprojected
--- openeo_processes_dask/process_implementations/cubes/utils.py
+++ openeo_processes_dask/process_implementations/cubes/utils.py
@@ -2,8 +2,12 @@ try:
     import dask
 except ImportError:
     dask = None
+import math
 from enum import Enum
-from math import pi
+
+from affine import Affine
+from odc.geo.geobox import GeoBox, resolution_from_affine
+
 
 def _has_dask():
     return dask is not None
@@ -14,30 +18,69 @@ def _is_dask_array(arr):
 
 
 class Unit(Enum):
-    metre = 'metre'
-    degree = 'degree'
+    metre = "metre"
+    degree = "degree"
 
-def approx_metres_2_degrees(length_metres = 10):
-    """ Angle of rotation calculated off the earths equatorial radius in metres. 
-        Ref: https://en.ans.wiki/5729/how-to-convert-meters-to-degrees/ """
+
+def approx_metres_2_degrees(length_metres=10):
+    """Angle of rotation calculated off the earths equatorial radius in metres.
+    Ref: https://en.ans.wiki/5729/how-to-convert-meters-to-degrees/"""
     earth_equator_radius_metres = 6378160
-    return (180 * length_metres ) / (earth_equator_radius_metres * pi)
+    return ((180 * length_metres) / (earth_equator_radius_metres * math.pi)) * 10
 
-def approx_degrees_2_metres(angle_in_degrees = 0.0009):
-    """ Calculated the metres travelled for an angle of rotation on the earths equatorial radius in metres.
-        Ref: https://en.ans.wiki/5728/how-to-convert-degrees-to-meters/ """
+
+def approx_degrees_2_metres(angle_in_degrees=0.0009):
+    """Calculated the metres travelled for an angle of rotation on the earths equatorial radius in metres.
+    Ref: https://en.ans.wiki/5728/how-to-convert-degrees-to-meters/"""
     earth_equator_radius_metres = 6378160
-    return (earth_equator_radius_metres * pi * angle_in_degrees) / 180
+    return ((earth_equator_radius_metres * math.pi * angle_in_degrees) / 180) / 10
 
-def detect_changing_unit(src_crs, dst_crs, src_res):
-    """ Is there a unit change between the src and dst CRS. If so, return the approximate value for the new resolution. """
+
+def detect_changing_unit(src_crs, dst_crs, src_res=None):
+    """Is there a unit change between the src and dst CRS. If so, return the approximate value for the new resolution."""
 
     src_unit = Unit(src_crs.axis_info[0].unit_name)
     dst_unit = Unit(dst_crs.axis_info[0].unit_name)
 
+    # If not dst_res has been set, check the src_unit and dst_unit to
+    # determine whether the src_res needs to be converted.
     if src_unit != dst_unit:
         if dst_unit == Unit.metre:
+            print("deg 2 met")
             return approx_degrees_2_metres(src_res)
         if dst_unit == Unit.degree:
+            print("met 2 deg")
             return approx_metres_2_degrees(src_res)
     return src_res
+
+
+def prepare_geobox(data, dst_crs, dst_res, new_top_left_x, new_top_left_y, scale=False):
+    """Get the destination geobox ready for the transformation."""
+    # Docs for geotransform
+    # https://gdal.org/tutorials/geotransforms_tut.html
+
+    new_affine = Affine(
+        dst_res,
+        0,
+        new_top_left_x,
+        0,
+        # Negative pixel width used for pixel height
+        -dst_res,
+        new_top_left_y,
+    )
+
+    if scale:
+        updated_resolution = resolution_from_affine(new_affine)
+
+        x_min, x_max = min(data.x.values), max(data.x.values)
+        y_min, y_max = min(data.y.values), max(data.y.values)
+
+        x_length = math.ceil(abs(x_min - x_max) / updated_resolution.x)
+        y_length = math.ceil(abs(y_min - y_max) / -updated_resolution.y)
+
+    else:
+        x_length = len(data.x)
+        y_length = len(data.y)
+
+    dst_geo = GeoBox(width=x_length, height=y_length, crs=dst_crs, affine=new_affine)
+    return dst_geo
