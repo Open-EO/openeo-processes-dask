87680cc2a4829a420a872a9a1d5d625180e2d550
sean <sean.hoyal@eodc.eu>
2023-05-15 08:49:30 +0200

    fix from pre commit

--- tests/test_resample.py
+++ tests/test_resample.py
@@ -1,16 +1,17 @@
 import numpy as np
 import pytest
-
 from pyproj.crs import CRS
-from openeo_processes_dask.process_implementations.cubes.resample import resample_spatial
+
+from openeo_processes_dask.process_implementations.cubes.resample import (
+    resample_spatial,
+)
 from tests.general_checks import general_output_checks
 from tests.mockdata import create_fake_rastercube
 
+
 @pytest.mark.parametrize("size", [(30, 30, 20, 4)])
 @pytest.mark.parametrize("dtype", [np.float32])
-def test_resample_spatial(
-    temporal_interval, bounding_box, random_raster_data
-):
+def test_resample_spatial(temporal_interval, bounding_box, random_raster_data):
     input_cube = create_fake_rastercube(
         data=random_raster_data,
         spatial_extent=bounding_box,
@@ -19,10 +20,7 @@ def test_resample_spatial(
         backend="dask",
     )
 
-    output_cube = resample_spatial(
-        data=input_cube,
-        epsg_code=32633
-        )
+    output_cube = resample_spatial(data=input_cube, epsg_code=32633)
 
     general_output_checks(
         input_cube=input_cube,
