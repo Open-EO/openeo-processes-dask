477104838cab1f338c82bdf73c8c5bc46c0402b1
ValentinaHutter <85164505+ValentinaHutter@users.noreply.github.com>
2023-05-19 13:23:03 +0200

    feat: add comparison processes (#50)
    * update pre-commit hook
    
    * update pre-commit
    
    * update pre-commit hook
    
    * update between test
    
    * pre-commit hook
    
    * pre-commit hook
    
    * pre commit hook
    
    * pre-commit hook
    
    * handle dict
    
    * handle array object
    
    * expose comparison processes
    
    * pre-commit hook
    
    * pre-commit hook
    
    * test between exclude max
    
    * update utils
    
    * update utils
    
    * update utils
    
    * update utils
    
    * update utils
    
    * update check_type
    
    * fix typo
    
    * update eq time
    
    * pre-commit hook
    
    * update test
    
    * pre-commit hook
    
    * update tests
    
    * pre-commit hook
    
    * pre-commit hook
    
    * pre-commit hook
    
    * add dask test
    
    * pre-commit hook
    
    * merge_cubes issue
    
    * update type checking test
    
    * pre-commit hook
    
    * remove check for time
    
    * update utils, only check one number in is_number
    
    * update comment
    
    * shorten code
    
    * add inf test
    
    * add is_infinite test
    
    * fix typo in test
    
    * update type checking logic
    
    * remove unnecessary check
    
    * wrap isnull with map_blocks
    
    * use np.where instead of da.where
    
    ---------
    
    Co-authored-by: Lukas Weidenholzer <lukas.weidenholzer@eodc.eu>

--- openeo_processes_dask/process_implementations/cubes/utils.py
+++ openeo_processes_dask/process_implementations/cubes/utils.py
@@ -4,6 +4,8 @@ try:
 except ImportError:
     dask = None
 
+from xarray.core.duck_array_ops import isnull as xr_isnull
+
 
 def _has_dask():
     return dask is not None
@@ -11,3 +13,14 @@ def _has_dask():
 
 def _is_dask_array(arr):
     return _has_dask() and isinstance(arr, dask.array.Array)
+
+
+def isnull(data):
+    if _is_dask_array(data):
+        return dask.array.map_blocks(xr_isnull, data)
+    else:
+        return xr_isnull(data)
+
+
+def notnull(data):
+    return ~isnull(data)
