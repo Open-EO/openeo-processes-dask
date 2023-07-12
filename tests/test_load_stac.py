3cf8360ad19fa7fc3a4ca9e830f23c555d9b2fd7
clausmichele <claus.michele@hotmail.it>
2023-07-12 09:41:27 +0200

    Using local file for test

--- tests/test_load_stac.py
+++ tests/test_load_stac.py
@@ -3,18 +3,16 @@ import pytest
 from openeo_processes_dask.process_implementations.cubes.load import load_stac
 
 
-def test_load_stac(temporal_interval, bounding_box):
-    url = (
-        "https://planetarycomputer.microsoft.com/api/stac/v1/collections/landsat-c2-l2"
-    )
+def test_load_stac(bounding_box):
+    url = "./tests/data/stac/s2_l2a_test_item.json"
     output_cube = load_stac(
         url=url,
         spatial_extent=bounding_box,
-        temporal_extent=temporal_interval,
         bands=["red"],
     )
 
     assert output_cube.openeo is not None
     assert len(output_cube[output_cube.openeo.x_dim]) > 0
     assert len(output_cube[output_cube.openeo.y_dim]) > 0
+    assert len(output_cube[output_cube.openeo.band_dims[0]]) > 0
     assert len(output_cube[output_cube.openeo.temporal_dims[0]]) > 0
