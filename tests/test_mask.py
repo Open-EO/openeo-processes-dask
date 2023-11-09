7c869ece6d20b85892be7e4d1d8426cfa56f8b1d
clausmichele <claus.michele@hotmail.it>
2023-11-09 13:47:24 +0100

    Improve code + tests

--- tests/test_mask.py
+++ tests/test_mask.py
@@ -1,9 +1,19 @@
+from functools import partial
 import numpy as np
 import pytest
-from openeo_pg_parser_networkx.pg_schema import TemporalInterval
-
+from openeo_pg_parser_networkx.pg_schema import (
+    TemporalInterval,
+    ParameterReference,
+)
 from openeo_processes_dask.process_implementations.cubes.mask_polygon import (
     mask_polygon,
+)
+from openeo_processes_dask.process_implementations.cubes.mask import (
+    mask,
+)
+from openeo_processes_dask.process_implementations.cubes.reduce import (
+    reduce_dimension,
+    reduce_spatial,
 )
 from tests.mockdata import create_fake_rastercube
 
@@ -29,3 +39,50 @@ def test_mask_polygon(
     assert np.isnan(output_cube).sum() > np.isnan(input_cube).sum()
     assert len(output_cube.y) == len(input_cube.y)
     assert len(output_cube.x) == len(input_cube.x)
+
+@pytest.mark.parametrize("size", [(30, 30, 20, 2)])
+@pytest.mark.parametrize("dtype", [np.float32])
+def test_mask(
+    temporal_interval,
+    bounding_box,
+    random_raster_data,
+    process_registry,
+):
+    """Test to ensure resolution gets changed correctly."""
+    input_cube = create_fake_rastercube(
+        data=random_raster_data,
+        spatial_extent=bounding_box,
+        temporal_extent=temporal_interval,
+        bands=["B02", "B03"],
+        backend="dask",
+    )
+
+    mask_cube = input_cube > 0
+    output_cube = mask(data=input_cube, mask=mask_cube)
+
+    assert np.isnan(output_cube).sum() > np.isnan(input_cube).sum()
+    assert len(output_cube.y) == len(input_cube.y)
+    assert len(output_cube.x) == len(input_cube.x)
+
+    _process = partial(
+        process_registry["max"].implementation,
+        ignore_nodata=True,
+        data=ParameterReference(from_parameter="data"),
+    )
+
+    mask_cube_no_x = reduce_dimension(data=mask_cube, dimension="x", reducer=_process)
+    with pytest.raises(Exception):
+        output_cube = mask(data=input_cube, mask=mask_cube_no_x)
+
+    # Mask should work without bands
+    mask_cube_no_bands = reduce_dimension(data=mask_cube, dimension="bands", reducer=_process)
+    output_cube = mask(data=input_cube, mask=mask_cube_no_bands)
+
+    # Mask should work without time
+    mask_cube_no_time = reduce_dimension(data=mask_cube, dimension="t", reducer=_process)
+    output_cube = mask(data=input_cube, mask=mask_cube_no_time)
+
+    # Mask should work without time and bands
+    mask_cube_no_time_bands = reduce_dimension(data=mask_cube_no_bands, dimension="t", reducer=_process)
+    output_cube = mask(data=input_cube, mask=mask_cube_no_time_bands)
+
