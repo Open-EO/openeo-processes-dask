2081ec2f0dfe9a950c8c64d3735b9a3d713fcff3
ValentinaHutter <valentina.hutter@eodc.eu>
2024-02-12 09:01:00 +0100

    run pre-commit hook

--- tests/test_math.py
+++ tests/test_math.py
@@ -2,8 +2,8 @@ import dask.array as da
 import numpy as np
 import pytest
 
-from openeo_processes_dask.process_implementations.math import *
 from openeo_processes_dask.process_implementations.exceptions import MinMaxSwapped
+from openeo_processes_dask.process_implementations.math import *
 
 
 def test_quantiles():
