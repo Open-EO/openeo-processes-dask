a54f4b1eb119e5aed1db70e086a209b14e39a8d1
Vincent Verelst <vincent.verelst@vito.be>
2025-01-07 12:11:26 +0100

    first implementation of local udf

---
+++ tests/test_udf.py
@@ -0,0 +1,38 @@
+import numpy as np
+import openeo
+import pytest
+import xarray as xr
+
+from openeo_processes_dask.process_implementations.udf.udf import run_udf
+from tests.general_checks import general_output_checks
+from tests.mockdata import create_fake_rastercube
+
+
+@pytest.mark.parametrize("size", [(6, 5, 4, 4)])
+@pytest.mark.parametrize("dtype", [np.float32])
+def test_run_udf(temporal_interval, bounding_box, random_raster_data):
+    input_cube = create_fake_rastercube(
+        data=random_raster_data,
+        spatial_extent=bounding_box,
+        temporal_extent=temporal_interval,
+        bands=["B02", "B03", "B04", "B08"],
+        backend="dask",
+    )
+
+    udf = """
+from openeo.udf import XarrayDataCube
+def apply_datacube(cube: XarrayDataCube, context: dict) -> XarrayDataCube:
+    return cube
+"""
+
+    output_cube = run_udf(data=input_cube, udf=udf, runtime="Python")
+
+    general_output_checks(
+        input_cube=input_cube,
+        output_cube=output_cube,
+        verify_attrs=True,
+        verify_crs=True,
+        expected_results=input_cube,
+    )
+
+    xr.testing.assert_equal(output_cube, input_cube)
